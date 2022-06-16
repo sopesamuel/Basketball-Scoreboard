634142838fde585c9a220cdd6d0599ebf7da5fc2
sopesamuel <104572982+sopesamuel@users.noreply.github.com>
2022-06-16 15:02:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let homescore1 = 0
+
+document.getElementById("score1").textContent = homescore1
+
+
+let scoreEl = document.getElementById("score1")
+
+function add() {
+
+let result = homescore1 + 1 
+scoreEl.textContent = result
+
+}
