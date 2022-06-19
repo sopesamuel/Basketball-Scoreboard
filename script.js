48a6b66e96833305e93d02e4b66bd228802cd9b0
sopesamuel <104572982+sopesamuel@users.noreply.github.com>
2022-06-19 05:23:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+let num1 = document.getElementById("score1")
+let num2 = document.getElementById("score2")
 
-let homescore1 = 0
+let sum1 = 0
+let sum2 = 0
 
-document.getElementById("score1").textContent = homescore1
+function add1() {
+    sum1 += 1
+    num1.textContent = sum1 
+}
 
+function add2(){
+    sum1 += 2
+    num1.textContent = sum1
+}
 
-let scoreEl = document.getElementById("score1")
+function add3(){
+    sum1 += 3
+    num1.textContent = sum1 
+}
 
-function add() {
+function increase1() {
+    sum2 += 1
+    num2.textContent = sum2 
+}
 
-let result = homescore1 + 1 
-scoreEl.textContent = result
+function increase2(){
+    sum2 += 2
+    num2.textContent = sum2
+}
 
+function increase3(){
+    sum2 += 3
+    num2.textContent = sum2 
 }
